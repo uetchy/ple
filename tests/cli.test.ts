5189b3ad0a4f1d88b4a88e84f16e4a6f099d2fbd
Yasuaki Uechi <y@uechi.io>
2020-11-04 12:55:32 +0900

    style: prettier

--- tests/cli.test.ts
+++ tests/cli.test.ts
@@ -1,10 +1,10 @@
-import execa from 'execa'
-import { join } from 'path'
+import execa from "execa";
+import { join } from "path";
 
-const pkg = require('../package.json')
-const bin = join(__dirname, '..', pkg.bin)
+const pkg = require("../package.json");
+const bin = join(__dirname, "..", pkg.bin);
 
-it('run', async () => {
-  const { stdout } = await execa(bin, ['--help'])
-  expect(stdout).toContain('Usage:')
-})
+it("run", async () => {
+  const { stdout } = await execa(bin, ["--help"]);
+  expect(stdout).toContain("Usage:");
+});
