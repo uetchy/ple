73745a4e9131287311da6c138e5182e052bf459e
Yasuaki Uechi <y@uechi.io>
2022-04-18 21:56:58 +0900

    feat: delete value by key

--- src/cli.ts
+++ src/cli.ts
@@ -18,16 +18,22 @@ function log(obj: any, json: boolean = false) {
 cli
   .command("<plistPath> [key] [value]")
   .option("-j, --json", "JSON output", { default: false })
-  .action((plistPath, key, value, { json }) => {
+  .option("-d, --delete-key", "Delete key from the plist", { default: false })
+  .action((plistPath, key, value, { json, deleteKey }) => {
     if (value) {
       rewritePlist(plistPath, key, value);
     } else {
-      log(readPlist(plistPath, key), json);
+      if (deleteKey) {
+        rewritePlist(plistPath, key, undefined);
+      } else {
+        log(readPlist(plistPath, key), json);
+      }
     }
   })
   .example("ple Info.plist")
   .example("ple Info.plist --json")
   .example("ple Info.plist bundleid")
-  .example("ple Info.plist version v7");
+  .example("ple Info.plist 'Frame MainWindow' '483 223'")
+  .example("ple Info.plist app.version -d");
 cli.help();
 cli.parse();
--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,76 @@
 import merge from "deepmerge";
+import { readFile, readFileSync } from "fs";
 import plist from "simple-plist";
 
-function objectFromDottedProps(key: string, value: any) {
+function objectFromDottedKey(key: string, value: any) {
   return key
     .split(".")
     .reverse()
-    .reduce((sum, key) => ({ [key]: sum }), value);
+    .reduce((v, key) => ({ [key]: v }), value);
 }
 
-function readDottedProps(key: string, obj: any): any {
+function readValueByDottedKey(key: string, obj: any): any {
   return key.split(".").reduce((sum, key) => {
     return sum[key];
   }, obj);
 }
 
-function loadPlist(plistPath: string) {
-  return plist.readFileSync(plistPath);
+function removeValueByDottedKey(obj: any, key: string) {
+  const keys = key.split(".");
+  keys.reduce((sum, key, i) => {
+    if (keys.length - 1 === i) {
+      delete sum[key];
+      return;
+    }
+    return sum[key];
+  }, obj);
+  return obj;
 }
 
-export function writePlist(plistPath: string, data: any): void {
-  plist.writeFileSync(plistPath, data);
+function isBplist(data: string | Buffer): boolean {
+  return data.slice(0, 6).toString() === "bplist";
+}
+
+function parsePlist<T = unknown>(data: string | Buffer): T {
+  return plist.parse(data);
+}
+
+export function writePlist(
+  plistPath: string,
+  data: any,
+  binary: boolean = false
+): void {
+  (binary ? plist.writeBinaryFileSync : plist.writeFileSync)(plistPath, data);
 }
 
 export function readPlist(plistPath: string, key?: string): any {
-  const data = loadPlist(plistPath);
+  const raw = readFileSync(plistPath);
+  const data = parsePlist(raw);
   if (!key) return data;
-  return readDottedProps(key, data);
+  return readValueByDottedKey(key, data);
 }
 
 export function rewritePlist(
   plistPath: string,
   key: string,
-  value: string
+  value?: string
 ): unknown {
-  const data = loadPlist(plistPath);
-  const newData = merge(data as any, objectFromDottedProps(key, value));
-  writePlist(plistPath, newData);
+  const raw = readFileSync(plistPath);
+  const isBinary = isBplist(raw);
+  const data = parsePlist(raw);
+  const newData =
+    value === undefined
+      ? removeValueByDottedKey(data as any, key)
+      : merge(data as any, objectFromDottedKey(key, tryParseNumber(value)));
+  writePlist(plistPath, newData, isBinary);
   return newData;
+}
+
+function tryParseNumber(numStr: string): string | number {
+  const maybeNumber = Number(numStr);
+  if (!Number.isNaN(maybeNumber)) {
+    return maybeNumber;
+  }
+
+  return numStr;
 }
